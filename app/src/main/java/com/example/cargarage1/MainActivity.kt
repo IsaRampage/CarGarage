017dec781e50558fa2bf41e63f4b213e4bd85d70
IsaRampage <steven.cichowski@web.de>
2023-01-11 15:58:10 +0100

    mainActivity vorbereitet + data binding ins layout

--- app/src/main/java/com/example/cargarage1/MainActivity.kt
+++ app/src/main/java/com/example/cargarage1/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.cargarage1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import de.syntaxinstitut.carGarage1.R
+import de.syntaxinstitut.carGarage1.databinding.ActivityMainBinding
 
+/**
+ * Main Activity, dient als Einstiegspunkt für die App
+ */
 class MainActivity : AppCompatActivity() {
+
+    /* -------------------- Klassen Variablen -------------------- */
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    /* -------------------- Lifecycle -------------------- */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        /// hello world bla
+
+        // Das Binding zur XML-Datei
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
